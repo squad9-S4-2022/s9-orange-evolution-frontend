12b217e613bbfa2cf1983d84bd737a01167928a9
Lucas Nabesima <lucas.nabesima@gmail.com>
2022-11-07 22:52:11 -0300

    style(accordion): add more styles to accordion component

--- components/Accordion/Accordion.js
+++ components/Accordion/Accordion.js
@@ -14,21 +14,25 @@ const Accordion = () => {
           <img src='/img/more.svg' alt='' />
         </summary>
         <div className={styles.item}>
-          <div>
-            <header className={styles.header}>
-              <p className={styles.header__title}>HTML Attribute</p>
-              <p className={styles.header__subtitle}>- Tempo estimado: 16 min.</p>
-            </header>
-            <div>
-              <p>Assunto abordado: </p>
-              <pre>
-                <code>&lt;input&gt;; &lt;form&gt;; &lt;body&gt;</code>
-              </pre>
+          <div className={styles.groupWrapper}>
+            <img src='/img/completed.svg' alt='' className={styles.icon} />
+            <div className={styles.group1}>
+              <header className={styles.header}>
+                <p className={styles.header__title}>HTML Attribute</p>
+                <p className={styles.header__subtitle}>
+                  - Tempo estimado: 16 min.
+                </p>
+              </header>
+              <div className={styles.topics}>
+                <p>Assunto abordado: </p>
+                <pre>
+                  <code> &lt;input&gt;; &lt;form&gt;; &lt;body&gt;</code>
+                </pre>
+              </div>
             </div>
-          </div>
-
-          <div>
-            <Button label='Glossário' />
+            <div className={styles.group2}>
+              <Button label='Glossário' />
+            </div>
           </div>
 
           <div className={styles.type_button}>
@@ -36,8 +40,87 @@ const Accordion = () => {
             <Button label='Iniciar' />
           </div>
         </div>
-        <div className={styles.item}>lorem</div>
-        <div className={styles.item}>lorem</div>
+        <div className={styles.item}>
+          <div className={styles.groupWrapper}>
+            <img src='/img/todo.svg' alt='' className={styles.icon} />
+            <div className={styles.group1}>
+              <header className={styles.header}>
+                <p className={styles.header__title}>HTML Attribute</p>
+                <p className={styles.header__subtitle}>
+                  - Tempo estimado: 16 min.
+                </p>
+              </header>
+              <div className={styles.topics}>
+                <p>Assunto abordado: </p>
+                <pre>
+                  <code> &lt;input&gt;; &lt;form&gt;; &lt;body&gt;</code>
+                </pre>
+              </div>
+            </div>
+            <div className={styles.group2}>
+              <Button label='Glossário' />
+            </div>
+          </div>
+
+          <div className={styles.type_button}>
+            <TypeShield label='video' />
+            <Button label='Iniciar' />
+          </div>
+        </div>
+        <div className={styles.item}>
+          <div className={styles.groupWrapper}>
+            <img src='/img/todo.svg' alt='' className={styles.icon} />
+            <div className={styles.group1}>
+              <header className={styles.header}>
+                <p className={styles.header__title}>HTML Attribute</p>
+                <p className={styles.header__subtitle}>
+                  - Tempo estimado: 16 min.
+                </p>
+              </header>
+              <div className={styles.topics}>
+                <p>Assunto abordado: </p>
+                <pre>
+                  <code> &lt;input&gt;; &lt;form&gt;; &lt;body&gt;</code>
+                </pre>
+              </div>
+            </div>
+            <div className={styles.group2}>
+              <Button label='Glossário' />
+            </div>
+          </div>
+
+          <div className={styles.type_button}>
+            <TypeShield label='podcast' />
+            <Button label='Iniciar' />
+          </div>
+        </div>
+        <div className={styles.item}>
+          <div className={styles.groupWrapper}>
+            <img src='/img/todo.svg' alt='' className={styles.icon} />
+            <div className={styles.group1}>
+              <header className={styles.header}>
+                <p className={styles.header__title}>HTML Attribute</p>
+                <p className={styles.header__subtitle}>
+                  - Tempo estimado: 16 min.
+                </p>
+              </header>
+              <div className={styles.topics}>
+                <p>Assunto abordado: </p>
+                <pre>
+                  <code> &lt;input&gt;; &lt;form&gt;; &lt;body&gt;</code>
+                </pre>
+              </div>
+            </div>
+            <div className={styles.group2}>
+              <Button label='Glossário' />
+            </div>
+          </div>
+
+          <div className={styles.type_button}>
+            <TypeShield label='curso' />
+            <Button label='Iniciar' />
+          </div>
+        </div>
       </details>
     </>
   );
