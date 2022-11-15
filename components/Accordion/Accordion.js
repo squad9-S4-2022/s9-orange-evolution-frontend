3147985e9b1517917724e217e64fa74ce455e3fd
Lucas Nabesima <lucas.nabesima@gmail.com>
2022-11-15 18:45:33 -0300

    refactor: refactor accordion layout

--- components/Accordion/Accordion.js
+++ components/Accordion/Accordion.js
@@ -20,28 +20,54 @@ const Accordion = ({ content, openGlossaryModal, openRedirectionModal }) => {
           <div className={styles.itemGroup}>
             {element.courses?.map((course, index) => (
               <div className={styles.item} key={`${course.courseName}${index}`}>
-                <div className={styles.groupWrapper}>
-                  {/* <div className={styles.checkboxDesktop}>
-                    
-                  </div> */}
-                  <div className={styles.header}>
+                <div className={styles.groupWrapperMobile}>
+                  <div className={styles.headerMobile}>
                     <Checkbox courseName={course.courseName} />
-                    <p className={styles.header__title}>{course.courseName}</p>
-                    <p className={styles.header__subtitle}>
+                    <p className={styles.headerMobile__title}>
+                      {course.courseName}
+                    </p>
+                    <p className={styles.headerMobile__subtitle}>
                       - Tempo estimado: {course.estimatedTime}.
                     </p>
                   </div>
-                  <div className={styles.topics}>
+                  <div className={styles.mobileTopics}>
                     <p>Assunto abordado: {course.coveredTopics}</p>
                   </div>
 
-                  <div className={styles.buttonControl}>
+                  <div className={styles.mobileButtonControl}>
                     <Button label='Glossário' onClick={openGlossaryModal} />
                     <TypeShield label={course.type} />
                     <Button label='Iniciar' onClick={openRedirectionModal} />
                   </div>
                 </div>
+
+                <div className={styles.groupWrapper}>
+                  <Checkbox courseName={course.courseName} />
+                  <div className={styles.info}>
+                    <div className={styles.header}>
+                      <p className={styles.header__title}>
+                        {course.courseName}
+                      </p>
+                      <p className={styles.header__subtitle}>
+                        - Tempo estimado: {course.estimatedTime}.
+                      </p>
+                    </div>
+
+                    <div className={styles.topics}>
+                      <p>
+                        <span>Assunto abordado:</span> {course.coveredTopics}
+                      </p>
+                    </div>
+                  </div>
+                  <Button label='Glossário' onClick={openGlossaryModal} />
+
+                  <div className={styles.buttonControl}>
+                    <TypeShield label={course.type} />
+                    <Button label='Iniciar' onClick={openRedirectionModal} />
+                  </div>
+                </div>
               </div>
+
               // <div className={styles.item} key={`${course.courseName}${index}`}>
               //   <div className={styles.groupWrapper}>
               //     <div className={styles.checkboxDesktop}>
